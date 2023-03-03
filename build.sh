70818608617949b2a06abd1ec1b709f6624627fb
Yuan, Xiaolan <xiaolan.yuan@intel.com>
2023-03-03 15:30:32 +0800

    add avx512 kernel

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 as -o sgemm_kernel_x64_fma.o sgemm_kernel_x64_fma.S
+as -o sgemm_kernel_x64_avx512.o sgemm_kernel_x64_avx512.S
 gcc -O3 -c main.c
-gcc -O3 -pthread -o sgemm_l1d sgemm_kernel_x64_fma.o main.o
+gcc -O3 -pthread -o sgemm_l1d sgemm_kernel_x64_fma.o sgemm_kernel_x64_avx512.o main.o
--- main.c
+++ main.c
@@ -19,6 +19,13 @@ void sgemm_kernel_x64_fma_m4n24(float *a,
     int m,
     int k);
 
+void sgemm_kernel_x64_avx512_m6n64(
+    float *a,
+    float *b,
+    float *c,
+    int m,
+    int k);
+
 #ifdef __cplusplus
 }
 #endif
@@ -91,7 +98,7 @@ int main(int argc, char *argv[])
 
     int m = atoi(argv[1]);
     int k = atoi(argv[2]);
-    long comp = 2L * m * k * 24L;
+    long comp = 2L * m * k * 64L;
     int loop_time = (int)(2e11 / comp);
 
     struct timespec start, end;
@@ -100,9 +107,9 @@ int main(int argc, char *argv[])
     thread_bind(0);
 
     float *a = (float*)page_alloc(m * k * sizeof(float));
-    float *b = (float*)page_alloc(k * 24 * sizeof(float));
-    float *c1 = (float*)page_alloc(m * 24 * sizeof(float));
-    float *c2 = (float*)page_alloc(m * 24 * sizeof(float));
+    float *b = (float*)page_alloc(k * 64 * sizeof(float));
+    float *c1 = (float*)page_alloc(m * 64 * sizeof(float));
+    float *c2 = (float*)page_alloc(m * 64 * sizeof(float));
 
     srand(time(NULL));
 
@@ -110,7 +117,7 @@ int main(int argc, char *argv[])
     {
         a[i] = (float)rand() / (float)RAND_MAX;
     }
-    for (i = 0; i < k * 24; i++)
+    for (i = 0; i < k * 64; i++)
     {
         b[i] = (float)rand() / (float)RAND_MAX;
     }
@@ -119,35 +126,39 @@ int main(int argc, char *argv[])
     // warm up
     for (i = 0; i < loop_time; i++)
     {
-        sgemm_kernel_x64_fma_m4n24(a, b, c2, m, k);
+        //sgemm_kernel_x64_fma_m4n24(a, b, c2, m, k);
+        sgemm_kernel_x64_avx512_m6n64(a, b , c2, m, k);
     }
     clock_gettime(CLOCK_MONOTONIC_RAW, &start);
     for (i = 0; i < loop_time; i++)
     {
-        sgemm_kernel_x64_fma_m4n24(a, b, c2, m, k);
+        //sgemm_kernel_x64_fma_m4n24(a, b, c2, m, k);
+        sgemm_kernel_x64_avx512_m6n64(a, b , c2, m, k);
     }
     clock_gettime(CLOCK_MONOTONIC_RAW, &end);
 
     t = get_time(&start, &end) / loop_time;
     gflops = (double)comp / t * 1e-9;
 
-    printf("sgemm_kernel_x64_fma(%d, %d, %d): time = %lf us, perf = %lf GFLOPS.\n", m, 24, k, t * 1e6, gflops);
-
-    memset(c1, 0, m * 24 * sizeof(float));
-    memset(c2, 0, m * 24 * sizeof(float));
-    sgemm_naive(a, b, c1, m, 24, k);
-    sgemm_kernel_x64_fma_m4n24(a, b, c2, m, k);
-    save_bin("naive.bin", c1, m * 24);
-    save_bin("tuned.bin", c2, m * 24);
+    //printf("sgemm_kernel_x64_fma(%d, %d, %d): time = %lf us, perf = %lf GFLOPS.\n", m, 24, k, t * 1e6, gflops);
+    printf("sgemm_kernel_x64_avx(%d, %d, %d): time = %lf us, perf = %lf GFLOPS.\n", m, 64, k, t * 1e6, gflops);
+    
+    memset(c1, 0, m * 64 * sizeof(float));
+    memset(c2, 0, m * 64 * sizeof(float));
+    sgemm_naive(a, b, c1, m, 64, k);
+    //sgemm_kernel_x64_fma_m4n24(a, b, c2, m, k);
+    sgemm_kernel_x64_avx512_m6n64(a, b , c2, m, k);
+    save_bin("naive.bin", c1, m * 64);
+    save_bin("tuned.bin", c2, m * 64);
 
     printf("sgemm_naive result: naive.bin\n");
-    printf("sgemm_kernel_x64_fma_m4n24 result: tuned.bin\n");
+    printf("sgemm_kernel_x64_avx512_m6n64 result: tuned.bin\n");
     printf("Use fp_diff(https://github.com/pigirons/fp_diff) to compare the results.\n");
 
     page_free(a, m * k * sizeof(float));
-    page_free(b, k * 24 * sizeof(float));
-    page_free(c1, m * 24 * sizeof(float));
-    page_free(c2, m * 24 * sizeof(float));
+    page_free(b, k * 64 * sizeof(float));
+    page_free(c1, m * 64 * sizeof(float));
+    page_free(c2, m * 64 * sizeof(float));
     
     return 0;
 }
